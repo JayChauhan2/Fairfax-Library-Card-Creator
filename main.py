abba94d1535598f9e4f7866839417ed584f28bc8
SoftwareDev <163558479+JayChauhan2@users.noreply.github.com>
2025-11-09 08:50:11 -0500

    Add Selenium script for library registration automation
    This script automates the registration process on the Fairfax County Public Library website using Selenium. It fills out the registration form with randomized user data and retrieves the username and password after submission.

---
+++ main.py
@@ -0,0 +1,69 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Launch browser
+driver = webdriver.Chrome()
+
+# Open a page
+driver.get("https://fcplcat.fairfaxcounty.gov/patronaccount/selfregister.aspx?ctx=1.1033.0.0.1")
+# print(driver.page_source) #vomit the html
+
+home_library = driver.find_element(By.NAME, "ctl00$BodyMainContent$lstBranches")
+zip_code = driver.find_element(By.NAME, "ctl00$BodyMainContent$txtZipCode")
+
+# Wrap it in a Select object
+select = Select(home_library)
+
+# Select by visible text (what the user sees in the dropdown)
+select.select_by_visible_text("City of Fairfax Regional Library")
+zip_code.send_keys("23059")
+
+submit_button = driver.find_element(By.NAME, "ctl00$BodyMainContent$btnSubmitBranchAndZip")
+submit_button.click()
+
+wait = WebDriverWait(driver, 2)  # wait up to 2 seconds
+new_element = wait.until(
+    EC.presence_of_element_located((By.NAME, "txtNameFirst")) #first name text input
+)
+print("New page loaded!")
+
+text = f"jay{random.randint(1, 10000)}"
+driver.find_element(By.NAME, "txtNameFirst").send_keys(text)
+driver.find_element(By.NAME, "txtNameLast").send_keys(text)
+
+driver.find_element(By.NAME, "txtBirthdatemm").send_keys("09")
+driver.find_element(By.NAME, "txtBirthdatedd").send_keys("30")
+driver.find_element(By.NAME, "txtBirthdateyyyy").send_keys("2008")
+
+driver.find_element(By.NAME, "txtStreet1").send_keys("11600 Herrick Ln")
+
+driver.find_element(By.NAME, "txtEmail").send_keys("hcps-chauhanjs@henricostudents.org")
+
+
+home_library = Select(driver.find_element(By.NAME, "txtUDF3")).select_by_visible_text("NO")
+
+driver.find_element(By.NAME, "txtUsername").send_keys(text)
+driver.find_element(By.NAME, "txtPassword").send_keys(text)
+driver.find_element(By.NAME, "txtVerification").send_keys(text)
+
+driver.find_element(By.NAME, "btnSubmitMainForm").click()
+
+wait = WebDriverWait(driver, 2)  # wait up to 2 seconds
+new_element = wait.until(
+    EC.presence_of_element_located((By.ID, "content")) #main content thing
+)
+print("New page loaded!")
+
+b_element = driver.find_element(
+    By.XPATH,
+    "(//form[@id='formMain']//div[contains(@class, 'content-container')][1]//b)[1]"
+)
+
+print("user: ", str(b_element.text))
+print("password: ", text)
+
+driver.quit()
